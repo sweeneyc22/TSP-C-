6ae58ddc45d4cda2c64934d92de8a22c2d3d9e8e
Colby <70588351+sweeneyc22@users.noreply.github.com>
2022-06-06 11:44:16 -0500

    Create matrix.h

---
+++ matrix.h
@@ -0,0 +1,12 @@
+
+#ifndef UNTITLED_MATRIX_H
+#define UNTITLED_MATRIX_H
+
+#include <vector>
+
+auto dice();
+std::vector<int> fillMatrix(std::vector<int> &emptyMatrix);
+void printMatrix(std::vector<int> &matrix);
+void saveMatrixToFile(std::vector<int> &matrix);
+
+#endif //UNTITLED_MATRIX_H
