53a0da16823176e2176bbe6006838024eea723ac
Colby <70588351+sweeneyc22@users.noreply.github.com>
2022-06-06 11:44:54 -0500

    Create io.cpp

---
+++ Created by Colby on 5/21/2022./io.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "io.h"
+
+
+int getMatrixSize()
+{
+    int size{};
+    std::cout << "Enter a matrix size to solve: ";
+    std::cin >> size;
+    return size;
+}
+
